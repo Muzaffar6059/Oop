a35c0c0ddeb7422a515b110bc81f509e4ca62230
Muzaffar6059 <mirzamuzu1@gmail.com>
2024-03-05 18:39:25 +0500

    there is found an error in the string copmare function and fixed it.

--- char Array functions/char Array functions.cpp
+++ char Array functions/char Array functions.cpp
@@ -3,10 +3,10 @@
  using namespace std;
 
  int strComp(const char[], const char[]);
- int strLeng(const char[]);
+ //int strLeng(const char[]);
 
  int main(){
-     char str1[50]="HelloW", str2[50]="Hellow";
+     char str1[50]="Hello", str2[50]="Hello";
      if(strComp(str1, str2)==0){
          cout<<"both string are equals"<<endl;
          }
@@ -21,34 +21,30 @@
      return 0;
  }
 
- int strLeng(const char arr[]) {
+ /*int strLeng(const char arr[]) {
      int i = 0;
      while (arr[i++] != '\0');
      return i;
- }
+ }*/
 
  // compare function without case sensitive 
  int strComp(const char str1[], const char str2[]){
      int i;
-     if (strLeng(str1) < strLeng(str2)) {
-         return -1;
-     }
-     else {
-         for (i = 0; str1[i] && str2[i]; i++)
+     for (i = 0; str1[i] || str2[i]; i++)
      {
          char c1 = tolower(str1[i]);
          char c2 = tolower(str2[i]);
-         if(c1 != c2)
+         if (c1 != c2) {
              return c1 - c2;
+         }
      }
-     return 0;    
-     }
+     return 0;
  }
  // compare function with case sensitive 
  /*
  int strComp(const char str1[], const char str2[]){
      int i;
-     for (i = 0; str1[i] && str2[i]; i++)
+     for (i = 0; str1[i] || str2[i]; i++)
      {
          if(str1[i]!=str2[i])
              return str1[i]-str2[i];
